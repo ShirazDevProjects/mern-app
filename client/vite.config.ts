e1ea5f4a65b8ed475055136643a13b74f289c7d4
ShirazDev <shirazj133@gmail.com>
2026-08-03 22:04:40 +0500

    updating vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    preview: {
+        allowedHosts: ['server.srv1011002.hstgr.cloud']
+    },
     server: {
         port: 4000,
         proxy: {
