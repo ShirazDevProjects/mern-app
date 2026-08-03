0e613a3273b287c6be568b6db3fe371fa09da772
ShirazDev <shirazj133@gmail.com>
2026-08-03 21:44:15 +0500

    updated vite.config with domain

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 4000,
         proxy: {
             '/api': {
-                target: 'https://server.srv1011002.hstgr.cloud',
+                target: 'https://server.websitemakerssolutions.com',
                 changeOrigin: true,
             },
         },
